4a793ec1bfc21d6a866bcb3b4c44e052256e2aa9
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-08-19 23:44:26 +0300

    create extension

--- weboodi.js
+++ weboodi.js
@@ -4,7 +4,7 @@ const list = document.querySelectorAll(
 
 let cumulativeOp = 0;
 
-stuff = [...list]
+const stuff = [...list]
   .map(item => [...item.querySelectorAll("td")])
   .filter(item => item.length)
   .map(item => item.map(value => value.textContent.trim()))
@@ -27,4 +27,105 @@ stuff = [...list]
     };
   });
 
-JSON.stringify(stuff);
+const yolo = '<canvas id="chart" width="500" height="200"></canvas>';
+
+const listaTaulukko = document.querySelectorAll("table")[1];
+listaTaulukko.outerHTML = listaTaulukko.outerHTML + yolo;
+
+const find = (list, key) => list.find(({ pvm }) => pvm === key);
+
+const grouped = stuff
+  .reduce((initial, item) => {
+    const found = find(initial, item.pvm);
+    if (!found) {
+      return [...initial, item];
+    }
+
+    return initial.map(jee => {
+      const today =
+        jee.pvm === item.pvm
+          ? {
+              cumulativeOp: item.op + jee.cumulativeOp,
+              op: item.op + jee.op
+            }
+          : null;
+
+      return {
+        ...jee,
+        ...today
+      };
+    });
+  }, [])
+  .map(item => ({ ...item, op: item.op * 10 }));
+
+const draw = ({ id, labels, datasets }) => {
+  var ctx = document.getElementById(id);
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: { labels, datasets },
+    options: {
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            console.log("tooltip", tooltipItem);
+
+            const label = data.datasets[tooltipItem.datasetIndex].label || "";
+
+            const value = Math.round(tooltipItem.yLabel * 100) / 100;
+
+            // datasetIndex = bar chart, values are multiplied by ten to show larger bars
+            const labelValue = tooltipItem.datasetIndex ? value : value / 10;
+
+            return `${label}: ${labelValue}`;
+          }
+        }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+};
+
+const style = {
+  backgroundColor: [
+    "rgba(255, 99, 132, 0.2)",
+    "rgba(54, 162, 235, 0.2)",
+    "rgba(255, 206, 86, 0.2)",
+    "rgba(75, 192, 192, 0.2)",
+    "rgba(153, 102, 255, 0.2)",
+    "rgba(255, 159, 64, 0.2)"
+  ],
+  borderColor: [
+    "rgba(255,99,132,1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 206, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
+    "rgba(255, 159, 64, 1)"
+  ],
+  borderWidth: 1
+};
+
+draw({
+  id: "chart",
+  labels: grouped.map(({ pvm }) => pvm),
+  datasets: [
+    {
+      label: "Päivän opintopisteet",
+      data: grouped.map(({ op }) => op)
+    },
+    {
+      label: "Suoritukset",
+      data: grouped.map(({ cumulativeOp }) => cumulativeOp),
+      ...style,
+      type: "line"
+    }
+  ]
+});
