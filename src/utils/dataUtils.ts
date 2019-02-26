9ed3e338fd05a83ef38038e1cdc295c78eb3b374
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2019-02-26 16:10:26 +0200

    Add studentNumber function

---
+++ src/utils/dataUtils.ts
@@ -0,0 +1,10 @@
+// TODO: this feature is for future
+// NOTE: because the extension is not running in the same thread as where the studentNumber exists
+// We have to hack around it to get the value using ugly document querySelector and matching strings
+// Sure we could use chrome extension api to access the window api but then the extension will break on other browsers
+// If you find a better (and crossbrowser) way to do this, please feel free to do it
+// UGH
+export const getOpiskelijanumero = (): string | null =>
+  (((document.querySelector('script') || { innerText: '' }).innerText.match(
+    /"studentNumber":"\d+"/,
+  ) || [''])[0].match(/\d+/) || ['0'])[0];
